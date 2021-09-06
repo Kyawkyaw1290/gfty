a347090ec35ebacce4689f4522d50a1aba0213fd
zeeone <82263175+zeeoneofc@users.noreply.github.com>
2021-09-06 18:25:39 +0800

    Create verif.js

---
+++ verif.js
@@ -0,0 +1,80 @@
+[
+`
+{
+"Verification code" : 1234,
+"Status" : False,
+"Message" : "Please refresh your browser"
+}
+`
+,
+`
+{
+"Verification code" : 1278,
+"Status" : False,
+"Message" : "Please refresh your browser"
+}
+`
+,
+`
+{
+"Verification code" : 7892,
+"Status" : False,
+"Message" : "Please refresh your browser"
+}
+`
+,
+`
+{
+"Verification code" : 6274,
+"Status" : False,
+"Message" : "Please refresh your browser"
+}
+`
+,
+`
+{
+"Verification code" : 8810,
+"Status" : False,
+"Message" : "Please refresh your browser"
+}
+`
+,
+`
+{
+"Verification code" : 3670,
+"Status" :True,
+"Message" : "Send this code to WhatsApp bot"
+}
+`
+,
+`
+{
+"Verification code" : 9093,
+"Status" :True,
+"Message" : "Send this code to WhatsApp bot"
+}
+`
+,
+`
+{
+"Verification code" : 6389,
+"Status" :True,
+"Message" : "Send this code to WhatsApp bot"
+}
+`
+,
+`
+{
+"Verification code" : 4629,
+"Status" :True,
+"Message" : "Send this code to WhatsApp bot"
+}
+`
+,
+`
+{
+"Verification code" : 1073,
+"Status" :True,
+"Message" : "Send this code to WhatsApp bot"
+}`
+]
